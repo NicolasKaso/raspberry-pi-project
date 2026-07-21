7d0ce1a9f1d7fea3f4c38704c0a73cbe194205fd
NicolasKaso <nicolas.kaso11@gmail.com>
2026-07-21 14:14:51 -0400

    started the spotify_page.py, fonts and colours are good

--- pages/spotify_page.py
+++ pages/spotify_page.py
@@ -1,10 +1,25 @@
 class SpotifyPage:
 
     def __init__(self):
-        pass
+       
+#fonts        
+        project_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+        font_path = os.path.join(project_root, "fonts", "ArchivoBlack-Regular.ttf")
+        
+        self.title_font = pygame.font.Font(font_path, 60)
+        self.artist_font = pygame.font.Font(font_path, 40)
+
+#colours
+
+        self.GREEN = (29, 185, 84)
+        self.GREY = (60, 60, 60)
+        self.LIGHT_GREY = (80, 80, 80)
 
     def update(self):
         pass
 
     def draw(self, screen, data):
-        pass
+        
+#background card
+        pygame.draw.rect(screen, self.GREY, (10, 10, 780, 460), border_radius = 25)
+
