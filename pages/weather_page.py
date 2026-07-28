9aeb209e8f36b7699cff199476ade89e337e015f
NicolasKaso <nicolas.kaso11@gmail.com>
2026-07-28 03:13:20 -0400

    started the weather_page.py file, static elements are good and the fonts are imported

--- pages/weather_page.py
+++ pages/weather_page.py
@@ -1,11 +1,38 @@
+import pygame
+import os
+
+
 class WeatherPage:
 
-    def __init__(self):
+    def __init__(self):                                             #INIT PART OF THE CODE
+        
+#variables
+        self.WHITE = (255, 255, 255)
+        self.GREY = (60, 60, 60)
+        
+
+#fonts
+        project_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+        font_path = os.path.join(project_root, "fonts", "ArchivoBlack-Regular.ttf") 
+        self.general_font = pygame.font.Font(font_path, 40)
+
+    def update(self, data):                                         #UPDATE PART OF THE CODE
         pass
 
-    def update(self, data):
-        pass
+
+    def draw(self, screen, data):                                   #DRAW PART OF THE CODE
+        
+#static elements
+        
+    #current weather background card
+        pygame.draw.rect(screen, self.GREY, (10, 10, 780, 260), border_radius = 25)
 
 
-    def draw(self, screen, data):
-        pass
+    #forecast weather background card
+        pygame.draw.rect(screen, self.GREY, (10, 280, 780, 190), border_radius = 25)
+
+
+#changing elements
+    
+#current weather
+        
